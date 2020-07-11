69893b27e374d28850d4dea100855f5fc8e95a15
Juan David Rendon <dadafe2014@gmail.com>
2020-07-11 16:34:27 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+
+var hbs = require('hbs');
+//helpers hbs
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use( express.static( __dirname + '/public'));
+
+//Express hbs
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.get('/', (req, res) =>{
+
+    res.render('home',{
+        nombre: 'juan david'
+    });
+})
+app.get('/about', (req, res) =>{
+
+    res.render('about');
+})
+ 
+// app.get('/', (req, res) =>{
+// //   res.send('Hello World')
+//     let salida ={
+//         nombre: 'juan david',
+//         edad: 41,
+//         url: req.url
+//     }
+//     res.send(salida);
+// })
+ 
+app.listen( port,()=>{
+    console.log(`Escuchando el puerto ${port}`);
+});
